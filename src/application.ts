4f8929e71279a48e2298a81961d097b8556caea1
RandomKiddo <nghugare2@outlook.com>
2021-03-27 21:48:00 -0400

    Started Building TS App

---
+++ src/application.ts
@@ -0,0 +1,34 @@
+import {
+    Discord,
+    On,
+    Client
+} from "@typeit/discord";
+import {
+    Message
+} from "discord.js";
+
+@Discord
+export class AppDiscord {
+    private static _client: Client;
+    private _prefix: string = "!";
+    static start() {
+        this._client = new Client();
+        this._client.login(
+            "YOUR_TOKEN_HERE",
+            `$(__dirname}/*Discord.ts`
+        );
+    }
+    @On("message")
+    async onMessage(message: Message, client: Client) {
+        if (AppDiscord._client.user.id !== message.author.id) {
+            const cmd = message.content.replace(this._prefix, "").toLowerCase();
+            switch (cmd) {
+                case "ping":
+                    //To Add Ping Logic
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
